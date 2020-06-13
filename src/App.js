4b138c24c3ead19dae158b33969ea6bf1654d1d2
SaqibHussain06 <65915320+SaqibHussain06@users.noreply.github.com>
2020-06-13 14:28:33 +0500

    added commit

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ export default function App() {
   return (
     <div className={`box ${isMorning ? 'dayLight' : ''}`}>
 
-      <h1>Good {isMorning ? 'Morning!' : 'Night!'}</h1>
+      <h1>Good {isMorning ? 'Morning!' : 'Night!'} from Saqib Hussain</h1>
 
       <Message counter={count} />
       <br />
